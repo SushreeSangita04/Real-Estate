ba8fa4fe90ad3427a76c1cf1abec9eec91022cb2
Sushree <sushreesangitaroutaray2004@gmail.com>
2025-01-28 15:00:18 +0530

    add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { signInStart,signInFailure,signInSuccess } from '../redux/user/userSlice'
+
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading,error}=useSelector((state)=>state.user);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const dispatch=useDispatch();
   const handleChange = (e) => {
-    setFormData({
+    setFormData({ 
       ...formData,
       [e.target.id]: e.target.value,
     });
@@ -15,7 +20,8 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
+      //setLoading(true);
       const res = await fetch('/api/auth/signin',
         {
           method: 'POST',
@@ -27,18 +33,20 @@ export default function SignIn() {
       );
       const data = await res.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
+        // setError(data.message);
+        // setLoading(false);
         return;
       }
-      setLoading(false);
-      setError(null);
+      // setLoading(false);
+      // setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     }
     catch (error) {
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
-    setError(error.message);
+    
   }
 
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   server:{
+
      proxy:{
       '/api':{
-     //   target:'https://localhost:3000',
-     target: 'http://127.0.0.1:3000',
-  
-     secure:false,
+        target: 'http://127.0.0.1:3000',      secure:false,
       },
      },
   },
