8e056b27435af43cd35230a685c3a726728efa6a
Sushree <sushreesangitaroutaray2004@gmail.com>
2025-01-31 15:00:22 +0530

    update profile in header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
-import Header from './components/Header'
+import Header from './Components/Header'
+import PrivateRoute from './Components/PrivateRoute'
 
 export default function App() {
   return (
@@ -16,7 +17,9 @@ export default function App() {
       <Route path='/sign-in' element={<Signin/>} />
       <Route path='/sign-up' element={<SignUp/>} />
       <Route path='/about' element={<About/>} />
+      <Route element={<PrivateRoute/>}>
       <Route path='/profile' element={<Profile/>} />
+      </Route>
       </Routes>
     </BrowserRouter>
   )
