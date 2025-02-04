27687a0000e2e13a569ad4136902512c4feb5550
Sushree <sushreesangitaroutaray2004@gmail.com>
2025-02-04 14:09:41 +0530

    user update api

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 export default function Profile() {
   const {currentUser}=useSelector((state)=>state.user);
-  return (
+  return(
    <div className='max-w-lg p-3 mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
