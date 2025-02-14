6afbc53823dd35a0c9579057c4e6673ab601da65
Sushree <sushreesangitaroutaray2004@gmail.com>
2025-02-14 13:14:25 +0530

    Create Listing API route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
@@ -10,10 +11,6 @@ const corsOptions = {
   origin: 'http://localhost:5173',  // Your frontend URL
   credentials: true,  // Allow cookies to be sent with requests
 };
-
-
-
-
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to mongoDB');
@@ -29,6 +26,7 @@ app.listen(3000, () => {
 });
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
+app.use('/api/listing',listingRouter);
 app.use(cors(corsOptions));
 //middleware
 app.use((err,req,res,next)=>{
