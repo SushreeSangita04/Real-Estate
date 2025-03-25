81d52a22d18a6f36c2fa5f57a2f9a1acb2c25d83
Sushree <sushreesangitaroutaray2004@gmail.com>
2025-03-25 14:12:06 +0530

    Complete About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='max-w-6xl px-4 py-20 mx-auto'>
+      <h1 className='mb-4 text-3xl font-bold text-slate-800 '>About Sush Vistas</h1>
+      <p className='mb-4 text-slate-700 '>Welcome to Sush Vistas Real Estate, where your dream property is within reach. We are a trusted and dynamic real estate agency dedicated to providing exceptional service in every aspect of the property journey. With years of experience, we specialize in residential, commercial, and luxury properties, offering a comprehensive range of services to help you buy, sell, and invest with confidence.</p>
+      <p className='mb-4 text-slate-700 '>Our team of expert real estate professionals is committed to understanding your needs, guiding you through each step of the process, and ensuring a smooth and successful experience. At Sush Vistas, we prioritize building lasting relationships, offering personalized solutions that align with your goals and values.</p>
+      <p className='mb-4 text-slate-700 '>Whether you're a first-time buyer, an investor, or looking to upgrade your lifestyle, we are here to help you find the perfect property that matches your vision. Our in-depth market knowledge, combined with cutting-edge technology and a passion for excellence, makes us the ideal partner for all your real estate needs.
+        <br />
+        Explore Sush Vistas Real Estate today, and let's embark on a journey toward turning your property dreams into reality.</p>
     </div>
   )
 }
